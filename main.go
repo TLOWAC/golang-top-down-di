f449951ea1185bf8c349a7853b95910e7db3eea1
TLOWAC <leechanghun55@gmail.com>
2024-03-14 05:41:12 +0900

    feat: db init 및 setup router 추가

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
-
+import (
+	router "go-playground/top-down-di/api/routes"
+	"go-playground/top-down-di/internal/db"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+
+	// DB Conn
+	db.ConnectDB()
+
+	// Router 셋팅
+	r := router.SetupRouter()
+
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
